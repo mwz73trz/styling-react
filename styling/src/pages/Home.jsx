23cd0dda6cf59e705bcda750081abd0e7216b2b7
mwz73trz <michaelzura@bellsouth.net>
2022-11-05 09:47:28 -0400

    complete

---
+++ styling/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import "../App.css";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Creating a Credit Card</h1>
+      <div className="creditcard">
+        <p className="bank">JP Morgan Chase</p>
+        <p className="card-numb">1234 1234 1234 1234</p>
+        <p className="card-name">Michael Zura</p>
+        <div className="circle-red"></div>
+        <div className="circle-yellow"></div>
+      </div>
+      <div className="circle1>"></div>
+      <div className="circle2>"></div>
+      <div className="circle3>"></div>
+    </div>
+  );
+};
+
+export default Home;
